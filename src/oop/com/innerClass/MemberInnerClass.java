a0592a7f0786d91d7c4d7a419baf6f403365f18e
sadanspace <yxq1575856216@gmail.com>
2019-09-11 22:30:39 +0800

    内部类实例化语法实践

--- src/oop/com/innerClass/MemberInnerClass.java
+++ src/oop/com/innerClass/MemberInnerClass.java
@@ -6,13 +6,15 @@ package src.oop.com.innerClass;
 1. 直接可以访问外部类的私有成员变量、成员方法
 2. 外部类不能直接访问内部类的成员变量、成员方法
 3. 内部类和外部类具有同名变量或方法时，默认先访问内部，此时要想访问外部信息，需要使用"外部类名.this"来调用
+4. 实例化内部类，需要使用"外部类.内部类 内部类变量名 = 外部类实例.new 内部类();"语法实现
 
 */
 
 public class MemberInnerClass {
     public static void main(String[] args) {
         MemberOuter memberOuter = new MemberOuter();
-        memberOuter.fun();
+        MemberOuter.Inner inner = memberOuter.new Inner();
+        inner.print();
     }
 
 }
@@ -33,5 +35,11 @@ class MemberOuter{
 
     private void memberInfo(){
         System.out.println("memberInfo");
+    }
+
+    public static void main(String[] args) {
+        MemberOuter memberOuter = new MemberOuter();
+        Inner inner = memberOuter.new Inner();
+        inner.print();
     }
 }
