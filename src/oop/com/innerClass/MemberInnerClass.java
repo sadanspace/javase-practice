ef07271787b2a01ae2dc1068ec9ae47f58634cd3
sadanspace <yxq1575856216@gmail.com>
2019-09-11 22:22:03 +0800

    内部类访问同名属性

--- src/oop/com/innerClass/MemberInnerClass.java
+++ src/oop/com/innerClass/MemberInnerClass.java
@@ -5,6 +5,7 @@ package src.oop.com.innerClass;
 成员内部类：
 1. 直接可以访问外部类的私有成员变量、成员方法
 2. 外部类不能直接访问内部类的成员变量、成员方法
+3. 内部类和外部类具有同名变量或方法时，默认先访问内部，此时要想访问外部信息，需要使用"外部类名.this"来调用
 
 */
 
@@ -19,9 +20,10 @@ public class MemberInnerClass {
 class MemberOuter{
     private String info="hello world";
     class Inner{
+        private String info = "inner info";
         public void print(){
             System.out.println(info);
-            memberInfo();
+            System.out.println(MemberOuter.this.info);
         }
     }
 
