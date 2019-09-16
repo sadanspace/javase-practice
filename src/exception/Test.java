423c02152da9e61fa682cb3496e32a051147e0f6
sadanspace <yxq1575856216@gmail.com>
2019-09-16 22:42:38 +0800

    try中有return，finally中没有

---
+++ src/exception/Test.java
@@ -0,0 +1,26 @@
+package src.exception;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(test());
+
+    }
+
+    static public int test(){
+        int num = 10;
+        try {
+            System.out.println("try");
+            return num += 80;  // 等效于 num+=80;return num
+        } catch (Exception e){
+            System.out.println("Exception");
+        } finally {
+            if (num > 20) {
+                System.out.println("num > 20: " + num);
+            }
+            System.out.println("finally");
+
+        }
+        System.out.println("out");
+        return num;
+    }
+}
