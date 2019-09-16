bee36ebeff7ce160f30ca1d83491678f05e73f7b
sadanspace <yxq1575856216@gmail.com>
2019-09-16 21:26:38 +0800

    多catch抛出异常，只有一个可捕获

--- src/exception/test.java
+++ src/exception/test.java
@@ -4,10 +4,16 @@ public class test {
     public static void main(String[] args) {
         try {
             System.out.println(1/0);
-        } catch (Exception e) {
+        } catch (ArithmeticException e){
+            System.out.println("ArithmeticException");
+            e.printStackTrace(); // 获取栈跟踪的信息
+            System.out.println(e.getMessage());  // 获取异常信息
+            throw e;
+        }
+         catch (Exception e) {
+             System.out.println("Exception");
             e.printStackTrace(); // 获取栈跟踪的信息
             System.out.println(e.getMessage());  // 获取异常信息
         }
-
     }
 }
