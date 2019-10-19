c2312b95fcd9bf2b1228b4a6157a868b20828cf1
sadanspace <yxq1575856216@gmail.com>
2019-10-19 11:21:22 +0800

    标准输入、标准输出、标准错误输出 demo

--- src/StreamDemo/PrintStreamDemo.java
+++ src/StreamDemo/PrintStreamDemo.java
@@ -1,20 +1,25 @@
 package src.StreamDemo;
 
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 
 public class PrintStreamDemo {
     public static void main(String[] args) {
-        PrintStream printStream = new PrintStream(System.out);
-        printStream.write(123);
-        try {
-            printStream.write("你好".getBytes());
+
+        try (
+                InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                PrintStream printStream = new PrintStream(System.out);
+                PrintStream printErrStream = new PrintStream(System.err)
+        ) {
+            String line = "";
+            while (! (line = bufferedReader.readLine()).equals("exit")){
+                printStream.printf("Std: %s", line);
+                printStream.append("\n");
+                printErrStream.printf("Err: %s", line);
+                printErrStream.println();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        printStream.println(true);
-        printStream.println("好");
-        printStream.close();
-
     }
 }
